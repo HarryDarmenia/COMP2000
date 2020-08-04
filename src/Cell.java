1439d8a995a42488c8d0212eb590bf8b3c101acd
HarryDarmenia <69173095+HarryDarmenia@users.noreply.github.com>
2020-08-04 12:43:05 +1000

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -3,8 +3,15 @@ import java.awt.*;
 
 public class Cell extends JFrame {
  
-	public static void paint(Graphics g, int i, int j, int size) {
-        g.setColor(Color.WHITE);
+	public static void paint(Graphics g, int i, int j, int size) { 
+        Point p = MouseInfo.getPointerInfo().getLocation();
+        int x = p.x;
+        int y = p.y;
+        if(i<x && x<=i+size && j<y && y<=j+size){
+                g.setColor(Color.GRAY);
+        }else{
+                g.setColor(Color.WHITE);
+        }
         g.fillRect(i, j, size, size);
         g.setColor(Color.BLACK);
         g.drawRect(i, j, size, size);
